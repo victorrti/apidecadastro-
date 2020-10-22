308419050f3f7c6913b3237a176574dc71012273
victor rodrigues pereira <victorrodriguesti@gmail.com>
2020-10-22 14:06:05 -0300

    feat: ensure SignUpController returns 400 if no name id provide

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
+
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*ts'],
+  collectCoverageFrom: ['<rootDit>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
